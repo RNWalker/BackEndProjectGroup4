40cb87b601c07b4eee96d7d9335cf727a21b969b
sarahoco014 <soconnor2@outlook.com>
2023-08-14 16:04:45 +0100

    adds application

---
+++ backend_project_group4/src/main/java/com/bnta/backend_project_group4/BackendProjectGroup4Application.java
@@ -0,0 +1,13 @@
+package com.bnta.backend_project_group4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProjectGroup4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendProjectGroup4Application.class, args);
+	}
+
+}
---
+++ backend_project_group4/src/test/java/com/bnta/backend_project_group4/BackendProjectGroup4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnta.backend_project_group4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendProjectGroup4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
