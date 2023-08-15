ed426363898c27c08b13c31b89648fe689d79b31
sarahoco014 <soconnor2@outlook.com>
2023-08-15 11:43:59 +0100

    adds annotations to food class

--- backend_project_group4/src/main/java/com/bnta/backend_project_group4/models/Food.java
+++ backend_project_group4/src/main/java/com/bnta/backend_project_group4/models/Food.java
@@ -1,12 +1,28 @@
 package com.bnta.backend_project_group4.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
 import java.util.ArrayList;
 
+@Entity
+@Table(name = "foods")
 public class Food {
 
+    @Column
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column
     private String name;
+
+    @Column
     private int nutritionValue;
+
+    @Column
+    @ManyToMany(mappedBy = "foods")
+    @JsonIgnoreProperties({"foods"})
     private ArrayList<Pet> pets;
 
     public Food(Long id, String name, int nutritionValue) {
