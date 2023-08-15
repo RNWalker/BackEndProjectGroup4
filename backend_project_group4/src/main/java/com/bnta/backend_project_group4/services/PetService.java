8a4e5a2ad6219aae909d0a8fcc6109abc4d0e8ac
sarahoco014 <soconnor2@outlook.com>
2023-08-15 15:13:57 +0100

    adds updatePet method in PetService

--- backend_project_group4/src/main/java/com/bnta/backend_project_group4/services/PetService.java
+++ backend_project_group4/src/main/java/com/bnta/backend_project_group4/services/PetService.java
@@ -1,12 +1,15 @@
 package com.bnta.backend_project_group4.services;
 
+import com.bnta.backend_project_group4.models.Food;
 import com.bnta.backend_project_group4.models.Pet;
 import com.bnta.backend_project_group4.models.PetDTO;
+import com.bnta.backend_project_group4.models.Toy;
 import com.bnta.backend_project_group4.repositories.PetRepository;
 import com.bnta.backend_project_group4.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,6 +30,7 @@ public class PetService {
         return petRepository.findById(id);
     }
 
+    // check later re passing in toys/food
     public Pet savePet(PetDTO petDTO, Long userId){
         Pet pet = new Pet(petDTO.getName(), petDTO.getSpecies(), petDTO.getHappinessLevel(), petDTO.getEnergyLevel(), userRepository.findById(userId).get());
         petRepository.save(pet);
@@ -36,5 +40,16 @@ public class PetService {
     public void deletePet(Long id){
         petRepository.deleteById(id);
     }
-    
+
+    public Pet updatePet(PetDTO petDTO, Long id){
+        Pet petToUpdate = petRepository.findById(id).get();
+        petToUpdate.setName(petDTO.getName());
+        petToUpdate.setSpecies(petDTO.getSpecies());
+        petToUpdate.setHappinessLevel(petDTO.getHappinessLevel());
+        petToUpdate.setEnergyLevel(petDTO.getEnergyLevel());
+        petToUpdate.setToys(new ArrayList<Toy>());
+        petToUpdate.setFoods(new ArrayList<Food>());
+        petRepository.save(petToUpdate);
+        return petToUpdate;
+    }
 }
