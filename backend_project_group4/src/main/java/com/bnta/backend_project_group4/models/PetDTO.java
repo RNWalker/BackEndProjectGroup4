b2f998d56e6e58b9d9fac0dd03ee43b3805fb81e
charlotteg1 <charlotte.gould3@gmail.com>
2023-08-15 11:03:24 +0100

    added getters and setters to petDTO

--- backend_project_group4/src/main/java/com/bnta/backend_project_group4/models/PetDTO.java
+++ backend_project_group4/src/main/java/com/bnta/backend_project_group4/models/PetDTO.java
@@ -11,4 +11,75 @@ public class PetDTO {
     private Long userId;
     private List<Long> toyIds;
     private List<Long> foodIds;
+
+    public PetDTO(String name, Species species, int happinessLevel, int energyLevel, Long userId, List<Long> toyIds, List<Long> foodIds) {
+        this.name = name;
+        this.species = species;
+        this.happinessLevel = happinessLevel;
+        this.energyLevel = energyLevel;
+        this.userId = userId;
+        this.toyIds = toyIds;
+        this.foodIds = foodIds;
+    }
+
+    // default constructor
+    public PetDTO() {}
+
+    //getters and setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Species getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(Species species) {
+        this.species = species;
+    }
+
+    public int getHappinessLevel() {
+        return happinessLevel;
+    }
+
+    public void setHappinessLevel(int happinessLevel) {
+        this.happinessLevel = happinessLevel;
+    }
+
+    public int getEnergyLevel() {
+        return energyLevel;
+    }
+
+    public void setEnergyLevel(int energyLevel) {
+        this.energyLevel = energyLevel;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public List<Long> getToyIds() {
+        return toyIds;
+    }
+
+    public void setToyIds(List<Long> toyIds) {
+        this.toyIds = toyIds;
+    }
+
+    public List<Long> getFoodIds() {
+        return foodIds;
+    }
+
+    public void setFoodIds(List<Long> foodIds) {
+        this.foodIds = foodIds;
+    }
 }
