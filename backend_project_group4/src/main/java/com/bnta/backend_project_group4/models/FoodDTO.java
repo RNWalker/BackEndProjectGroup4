80e6c2bc5bd6951c6355b2da454d6cda84bb7986
charlotteg1 <charlotte.gould3@gmail.com>
2023-08-15 11:05:51 +0100

    added properties,getters, setters and constructor to FoodDTO

--- backend_project_group4/src/main/java/com/bnta/backend_project_group4/models/FoodDTO.java
+++ backend_project_group4/src/main/java/com/bnta/backend_project_group4/models/FoodDTO.java
@@ -1,4 +1,46 @@
 package com.bnta.backend_project_group4.models;
 
+import java.util.List;
+
 public class FoodDTO {
+
+    private String name;
+    private int nutritionValue;
+    private List<Long> petIds;
+
+    public FoodDTO(String name, int nutritionValue, List<Long> petIds) {
+        this.name = name;
+        this.nutritionValue = nutritionValue;
+        this.petIds = petIds;
+    }
+
+    //default constructor
+
+    public FoodDTO() {}
+
+    //getters and setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNutritionValue() {
+        return nutritionValue;
+    }
+
+    public void setNutritionValue(int nutritionValue) {
+        this.nutritionValue = nutritionValue;
+    }
+
+    public List<Long> getPetIds() {
+        return petIds;
+    }
+
+    public void setPetIds(List<Long> petIds) {
+        this.petIds = petIds;
+    }
 }
