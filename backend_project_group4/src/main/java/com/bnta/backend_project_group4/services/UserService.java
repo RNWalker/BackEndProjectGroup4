3ba53bd19e7617ef75204fc438e3f4931d619d4f
charlotteg1 <charlotte.gould3@gmail.com>
2023-08-15 12:32:31 +0100

    added update user and delete user

--- backend_project_group4/src/main/java/com/bnta/backend_project_group4/services/UserService.java
+++ backend_project_group4/src/main/java/com/bnta/backend_project_group4/services/UserService.java
@@ -8,6 +8,7 @@ import com.bnta.backend_project_group4.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -35,6 +36,24 @@ public class UserService {
             user.addPet(pet);
         }
         return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id){
+        // ? delete food from pet, toy from pet. then pet from user
+        userRepository.deleteById(id);
+    }
+
+    public User updateUser(UserDTO userDTO, Long id){
+        User userToUpdate= userRepository.findById(id).get();
+        userToUpdate.setName(userDTO.getName());
+        userToUpdate.setEmail(userDTO.getEmail());
+        userToUpdate.setPets(new ArrayList<Pet>());
+        for(Long petId: userDTO.getPetIds()){
+            Pet pet= petRepository.findById(petId).get();
+            userToUpdate.addPet(pet);
+        }
+        userRepository.save(userToUpdate);
+        return userToUpdate;
     }
 
 }
