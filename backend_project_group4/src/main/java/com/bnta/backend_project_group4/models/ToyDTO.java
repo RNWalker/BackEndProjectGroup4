e714d6f95a11e0e5d8bee945945f4c3711b50e84
charlotteg1 <charlotte.gould3@gmail.com>
2023-08-15 11:07:48 +0100

    added properties,getters, setters and constructor to ToyDTO

--- backend_project_group4/src/main/java/com/bnta/backend_project_group4/models/ToyDTO.java
+++ backend_project_group4/src/main/java/com/bnta/backend_project_group4/models/ToyDTO.java
@@ -1,4 +1,44 @@
 package com.bnta.backend_project_group4.models;
 
+import java.util.List;
+
 public class ToyDTO {
+
+    private String name;
+    private int happinessValue;
+    private List<Long> petIds;
+
+    public ToyDTO(String name, int happinessValue, List<Long> petIds) {
+        this.name = name;
+        this.happinessValue = happinessValue;
+        this.petIds = petIds;
+    }
+
+    // default constructor
+    public ToyDTO() {}
+
+    // getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHappinessValue() {
+        return happinessValue;
+    }
+
+    public void setHappinessValue(int happinessValue) {
+        this.happinessValue = happinessValue;
+    }
+
+    public List<Long> getPetIds() {
+        return petIds;
+    }
+
+    public void setPetIds(List<Long> petIds) {
+        this.petIds = petIds;
+    }
 }
