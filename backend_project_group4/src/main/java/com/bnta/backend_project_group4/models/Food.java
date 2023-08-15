1ea408a1e09a4765011371e6aafef7c4acf33d29
sarahoco014 <soconnor2@outlook.com>
2023-08-15 10:44:57 +0100

    adds properties, getters and setters for food

--- backend_project_group4/src/main/java/com/bnta/backend_project_group4/models/Food.java
+++ backend_project_group4/src/main/java/com/bnta/backend_project_group4/models/Food.java
@@ -1,4 +1,54 @@
 package com.bnta.backend_project_group4.models;
 
+import java.util.ArrayList;
+
 public class Food {
+
+    private Long id;
+    private String name;
+    private int nutritionValue;
+    private ArrayList<Pet> pets;
+
+    public Food(Long id, String name, int nutritionValue) {
+        this.id = id;
+        this.name = name;
+        this.nutritionValue = nutritionValue;
+        this.pets = new ArrayList<>();
+    }
+
+    // default constructor
+    public Food(){}
+
+    // getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNutritionValue() {
+        return nutritionValue;
+    }
+
+    public void setNutritionValue(int nutritionValue) {
+        this.nutritionValue = nutritionValue;
+    }
+
+    public ArrayList<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(ArrayList<Pet> pets) {
+        this.pets = pets;
+    }
 }
