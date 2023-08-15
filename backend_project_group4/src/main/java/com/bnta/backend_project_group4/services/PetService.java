843105df7bceb0aef4dfa5d290d2ffc87a175ccf
sarahoco014 <soconnor2@outlook.com>
2023-08-15 14:53:12 +0100

    adds findBy methods to PetService

--- backend_project_group4/src/main/java/com/bnta/backend_project_group4/services/PetService.java
+++ backend_project_group4/src/main/java/com/bnta/backend_project_group4/services/PetService.java
@@ -1,4 +1,26 @@
 package com.bnta.backend_project_group4.services;
 
+import com.bnta.backend_project_group4.models.Pet;
+import com.bnta.backend_project_group4.repositories.PetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class PetService {
+
+    @Autowired
+    PetRepository petRepository;
+
+    public List<Pet> getAllPets(){
+        return petRepository.findAll();
+    }
+
+    public Optional<Pet> getPetById(Long id){
+        return petRepository.findById(id);
+    }
+
+    
 }
