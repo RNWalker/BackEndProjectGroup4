3244a9a3851c2124f475212121d3c4a948ea62c1
darkpoetOX <ayindejam2009@yahoo.com>
2023-08-15 12:17:19 +0100

    working on saveUser in UserService

--- backend_project_group4/src/main/java/com/bnta/backend_project_group4/services/UserService.java
+++ backend_project_group4/src/main/java/com/bnta/backend_project_group4/services/UserService.java
@@ -1,6 +1,9 @@
 package com.bnta.backend_project_group4.services;
 
+import com.bnta.backend_project_group4.models.Pet;
 import com.bnta.backend_project_group4.models.User;
+import com.bnta.backend_project_group4.models.UserDTO;
+import com.bnta.backend_project_group4.repositories.PetRepository;
 import com.bnta.backend_project_group4.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,12 +17,24 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    PetRepository petRepository;
+
     public List<User> getAllUsers(){
         return userRepository.findAll();
     }
 
     public Optional<User> getUserById(Long id){
         return userRepository.findById(id);
+    }
+
+    public User saveUser(UserDTO userDTO){
+        User user = new User(userDTO.getName(), userDTO.getEmail());
+        for(Long petId: userDTO.getPetIds()){
+            Pet pet = petRepository.findById(petId).get();
+            user.addPet(pet);
+        }
+        return userRepository.save(user);
     }
 
 }
