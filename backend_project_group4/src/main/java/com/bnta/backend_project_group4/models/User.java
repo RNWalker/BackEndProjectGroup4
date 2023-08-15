42fe37e1250e5b8698fcafafd47694f23b4ab562
sarahoco014 <soconnor2@outlook.com>
2023-08-15 10:42:13 +0100

    adds properties, getters and setters for user

--- backend_project_group4/src/main/java/com/bnta/backend_project_group4/models/User.java
+++ backend_project_group4/src/main/java/com/bnta/backend_project_group4/models/User.java
@@ -1,4 +1,54 @@
 package com.bnta.backend_project_group4.models;
 
+import java.util.ArrayList;
+
 public class User {
+
+    private String name;
+    private Long id;
+    private String email;
+    private ArrayList<Pet> pets;
+
+    public User(String name, Long id, String email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.pets = new ArrayList<>();
+    }
+
+    // default constructor
+    public User(){}
+
+    // getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public ArrayList<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(ArrayList<Pet> pets) {
+        this.pets = pets;
+    }
 }
