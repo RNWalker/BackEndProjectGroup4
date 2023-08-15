2a3eaabf9a1e3daa149a8e21592e9048c86e2afe
darkpoetOX <ayindejam2009@yahoo.com>
2023-08-15 10:59:26 +0100

    added properties to PetDTO

--- backend_project_group4/src/main/java/com/bnta/backend_project_group4/models/PetDTO.java
+++ backend_project_group4/src/main/java/com/bnta/backend_project_group4/models/PetDTO.java
@@ -1,4 +1,14 @@
 package com.bnta.backend_project_group4.models;
 
+import java.util.List;
+
 public class PetDTO {
+
+    private String name;
+    private Species species;
+    private int happinessLevel;
+    private int energyLevel;
+    private Long userId;
+    private List<Long> toyIds;
+    private List<Long> foodIds;
 }
