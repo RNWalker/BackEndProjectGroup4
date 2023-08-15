87c0561248353aa545da5a892e08101e18c096a0
charlotteg1 <charlotte.gould3@gmail.com>
2023-08-15 10:12:53 +0100

    add idea

--- backend_project_group4/src/main/java/com/bnta/backend_project_group4/repositories/ToyRepository.java
+++ backend_project_group4/src/main/java/com/bnta/backend_project_group4/repositories/ToyRepository.java
@@ -1,4 +1,9 @@
 package com.bnta.backend_project_group4.repositories;
 
-public interface ToyRepository {
+import com.bnta.backend_project_group4.models.Toy;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ToyRepository extends JpaRepository<Toy, Long> {
 }
