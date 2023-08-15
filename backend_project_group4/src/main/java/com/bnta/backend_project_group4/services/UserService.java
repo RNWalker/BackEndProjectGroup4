0d638b4232644ca40747014e2063a5a6c3bf7489
darkpoetOX <ayindejam2009@yahoo.com>
2023-08-15 12:05:37 +0100

    added findById and findAll methods to user services

--- backend_project_group4/src/main/java/com/bnta/backend_project_group4/services/UserService.java
+++ backend_project_group4/src/main/java/com/bnta/backend_project_group4/services/UserService.java
@@ -1,4 +1,25 @@
 package com.bnta.backend_project_group4.services;
 
+import com.bnta.backend_project_group4.models.User;
+import com.bnta.backend_project_group4.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id){
+        return userRepository.findById(id);
+    }
+
 }
