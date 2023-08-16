74e57b2f06ed3187579006c819402f3419fec712
darkpoetOX <ayindejam2009@yahoo.com>
2023-08-16 12:25:13 +0100

    added get mapping for user controller

--- backend_project_group4/src/main/java/com/bnta/backend_project_group4/controllers/UserController.java
+++ backend_project_group4/src/main/java/com/bnta/backend_project_group4/controllers/UserController.java
@@ -1,4 +1,39 @@
 package com.bnta.backend_project_group4.controllers;
 
+import com.bnta.backend_project_group4.models.User;
+import com.bnta.backend_project_group4.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("users")
 public class UserController {
+    @Autowired
+    UserService userService;
+
+    //INDEX
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers(){
+        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+    }
+
+    //SHOW
+    @GetMapping(value= "/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id){
+        Optional<User> foundUser = userService.getUserById(id);
+        if(foundUser.isPresent()){
+            return new ResponseEntity<>(foundUser.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 }
