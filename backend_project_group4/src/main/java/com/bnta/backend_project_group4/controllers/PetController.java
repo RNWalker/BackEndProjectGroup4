24634fa90ae48a0f60dc88ce3f70fe96ba50e448
RNWalker <rebecca.noelia.walker@gmail.com>
2023-08-17 09:44:48 +0100

    started pet controller

--- backend_project_group4/src/main/java/com/bnta/backend_project_group4/controllers/PetController.java
+++ backend_project_group4/src/main/java/com/bnta/backend_project_group4/controllers/PetController.java
@@ -1,4 +1,16 @@
 package com.bnta.backend_project_group4.controllers;
 
+import com.bnta.backend_project_group4.services.PetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("pets")
 public class PetController {
+
+    @Autowired
+    PetService petService;
+
+    
 }
