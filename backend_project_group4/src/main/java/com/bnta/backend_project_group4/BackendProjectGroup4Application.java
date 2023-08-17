c87a3a2a25dbc8977c7ea84b23615f0f1c7ddf45
darkpoetOX <ayindejam2009@yahoo.com>
2023-08-17 12:26:19 +0100

    added Scheduling annotation to main application

--- backend_project_group4/src/main/java/com/bnta/backend_project_group4/BackendProjectGroup4Application.java
+++ backend_project_group4/src/main/java/com/bnta/backend_project_group4/BackendProjectGroup4Application.java
@@ -2,8 +2,10 @@ package com.bnta.backend_project_group4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BackendProjectGroup4Application {
 
 	public static void main(String[] args) {
