6e8646c4f3473563c9d5eb373e63eb045e686219
sarahoco014 <soconnor2@outlook.com>
2023-08-17 09:47:44 +0100

    adds index get mapping for pet controller

--- backend_project_group4/src/main/java/com/bnta/backend_project_group4/controllers/PetController.java
+++ backend_project_group4/src/main/java/com/bnta/backend_project_group4/controllers/PetController.java
@@ -1,9 +1,16 @@
 package com.bnta.backend_project_group4.controllers;
 
+import com.bnta.backend_project_group4.models.Pet;
+import com.bnta.backend_project_group4.models.Toy;
 import com.bnta.backend_project_group4.services.PetService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("pets")
@@ -11,6 +18,12 @@ public class PetController {
 
     @Autowired
     PetService petService;
+
+    //INDEX
+    @GetMapping
+    public ResponseEntity<List<Pet>> getAllPets(){
+        return new ResponseEntity<>(petService.getAllPets(), HttpStatus.OK);
+    }
 
     
 }
